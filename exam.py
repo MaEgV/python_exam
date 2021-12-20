9c5738171251d689b7e1c2e1381c037a883e21f1
MaEgV <60978225+MaEgV@users.noreply.github.com>
2021-12-20 11:25:36 +0300

    Add files via upload

--- exam.py
+++ exam.py
@@ -45,26 +45,31 @@ def c_n_k(n, k):
     return int(f(n)/(f(n-k)*f(k)))
 
 
-def adj_components(bridge, components):
-    first, second = False, False
+def count_pairs(component):
+    if len(component) == 1:
+        return 0
+    return 2 * c_n_k(len(component), 2)
+
+
+def adj_component(node, components):
     for component in components:
-        if not first and bridge[0] in component:
-            first = component
-        if not second and bridge[1] in component:
-            second = component
-    return first, second
+        if node in component:
+            return component
 
 
 def task2(n, edges):
     res = 0
     g = get_graph(n, edges)
-    components, bridges =  split_graph(g)
-    res += sum([2 * c_n_k(len(comp), 2) for comp in components])
+    components, bridges = split_graph(g)
+    res += sum([count_pairs(comp) for comp in components])
     for bridge in bridges:
-        first, second = adj_components(bridge, components)
-        res += len(first)
+        res += len(adj_component(bridge[0], components))
     return res
+
 
 print(task2(5, edges=[(1, 2, 2), (2, 3, 1), (3, 4, 2), (5, 2, 2)]))
 print(task2(6, edges=[(1, 2, 2), (2, 3, 1), (3, 4, 2), (5, 2, 2), (3, 6, 2)]))
 print(task2(6, edges=[(1, 2, 2), (2, 3, 1), (3, 4, 2), (5, 2, 2), (3, 6, 2), (1, 6, 1)]))
+print(task2(3, edges=[(1, 2, 2), (1, 3, 1)]))
+print(task2(2, edges=[(1, 2, 2)]))
+print(task2(2, edges=[(1, 2, 1)]))
